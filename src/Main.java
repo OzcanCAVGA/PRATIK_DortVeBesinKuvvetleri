d4ddb6e0528cb9787b8d564eac0464375dfa1d8b
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:00:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+/*
+Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.
+ */
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner yazici = new Scanner(System.in);
+        int sayi, ikinciSayi;
+        System.out.println("Sayi girin: ");
+        System.out.println("Sinir degerini gir: ");
+        sayi = yazici.nextInt();
+        for (int i = 4; i <=sayi ; i*=4) {
+            System.out.println("4'un kati: "+i);
+
+        }
+        for (int i = 5; i <=sayi ; i*=5) {
+            System.out.println("5'un kati: "+i);
+
+        }
+
+
+
     }
 }
